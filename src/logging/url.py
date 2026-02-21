642d5cf859e354584de44a0680d6aaa6043645ab
adelard07 <adelard@eduvance.in>
2026-02-21 19:46:14 +0530

    Enhance log ingestion endpoint: update router tags, change endpoint path to /ingest, and add redis_flush_count parameter to LogIngestionService

--- src/logging/url.py
+++ src/logging/url.py
@@ -3,16 +3,18 @@ from fastapi.responses import JSONResponse
 
 from src.logging.ingestion import LogIngestionService
 
-router = APIRouter(prefix="/logging", tags=["cloud", "logging"])
+router = APIRouter(prefix="/logging", tags=["cloud", "logging", "ingestion"])
 
-@router.post("/log")
+@router.post("/ingest")
 async def log(request: Request):
     try:
         data = await request.json()
 
-        ingestion = LogIngestionService()
-        log_object = ingestion.ingest_log(data)
+        ingestion_service = LogIngestionService(internal_batch_size=1, redis_flush_count=10)
+        log_object = ingestion_service.ingest_log(data)
         
-        return JSONResponse(content={"message": "Log received successfully"}, status_code=200)
+        return JSONResponse(content={"message": "Log received successfully", "log_object": log_object}, status_code=200)
+    
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
